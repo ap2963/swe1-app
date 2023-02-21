cac319e6709560658657e9e28c89bac08a3e06aa
agnespark824 <agnespark824@gmail.com>
2023-02-20 21:50:51 -0500

    downgraded python and updated requirements.txt

--- swe1app/settings.py
+++ swe1app/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 import os
 
 from pathlib import Path
-from env import *
+from dotenv import dotenv_values
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
