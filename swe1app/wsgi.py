a6f343007b735b2f3d97a3e1c1154a67361d0f4b
agnespark824 <agnespark824@gmail.com>
2023-02-21 02:31:40 -0500

    Updated requirements.txt to address ppsycopg2 dependency issue

--- swe1app/wsgi.py
+++ swe1app/wsgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
-import os
-
+import os, sys
 from django.core.wsgi import get_wsgi_application
+
+# add the project path into the sys.path
+project_path = Path(__file__).resolve().parent
+sys.path.append(project_path)
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append('/var/app/venv/*/bin/activate/lib/python3.7/site-packages')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swe1app.settings')
 
 application = get_wsgi_application()
+
